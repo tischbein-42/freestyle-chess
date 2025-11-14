01a9763098fa46da6066bcdfdd22e06b32cd95d8
Paul Bachmann <paulbachmann@MacBook-Air-von-Paul.local>
2025-11-14 12:06:58 +0100

    Initial commit

---
+++ FreestyleChess/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FreestyleChess.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
